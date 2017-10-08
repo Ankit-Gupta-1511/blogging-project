f1765e6d3598ea3de840a12bb68a049c62b7dbcc
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2017-10-08 23:25:05 +0530

    blog page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(
     function ()
-{        carouselImages();}
+              {
+                    carouselImages();
+                    populateTweets();
+              }
 );
 
 function carouselImages()
 {
     var items = $(".item");
     var image;
-    
+
     $(".item .content").each(function(index){
                     putImage($(this));
                     });
@@ -19,4 +22,38 @@ function putImage(item)
     item.css({
         "background-image" : 'url('+imageUrl+')',
     });
+}
+
+
+// twitter sharing api
+
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+
+// populating tweets
+
+function populateTweets()
+{
+    var currentUrl = $(".twitter-btn").attr("href");
+    var heading = $(".blog-heading").text();
+    var author = $(".blog-author").text();
+    var updatedUrl = currentUrl+"?text="+heading+" - "+author;
+    console.log(updatedUrl);
+    $(".twitter-btn").attr("href",updatedUrl);
 }
