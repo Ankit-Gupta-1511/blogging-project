1809844e889c73be6f20b5be7752e3ba2557a381
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2017-10-09 19:56:46 +0530

    linked all files

--- js/app.js
+++ js/app.js
@@ -3,8 +3,14 @@ $(document).ready(
               {
                     carouselImages();
                     populateTweets();
+                    newsletterPopup();
+                    setTimeout(function(){
+                            $("#subscribeModal.modal").modal("show");
+                    },2000);
               }
 );
+
+
 
 function carouselImages()
 {
@@ -56,4 +62,57 @@ function populateTweets()
     var updatedUrl = currentUrl+"?text="+heading+" - "+author;
     console.log(updatedUrl);
     $(".twitter-btn").attr("href",updatedUrl);
+}
+
+
+
+//////////////////////////
+// Pop Up Element
+/////////////////////////
+
+
+function newsletterPopup()
+{
+  var popup = '<div class="popup"><div class="panel panel-default">'+
+                  '<div class="panel-heading">Subscribe</div>'+
+                  '<div class="panel-body">'+'<p>Subscribe to our newsletter</p>'+'<button class="btn btn-default normal-cta" type="button" data-toggle="modal" data-target="#subscribeModal">Subscribe</button>'+'</div>'+
+              '</div></div>';
+
+  var subscribeModal = '<div id="subscribeModal" class="modal fade" role="dialog">'+
+  '<div class="modal-dialog">'+
+
+    '<div class="modal-content">'+
+      '<div class="modal-header">'+
+        '<button type="button" class="close" data-dismiss="modal">&times;</button>'+
+        '<h4 class="modal-title">Subscribe to our newsletter</h4>'+
+      '</div>'+
+      '<div class="modal-body">'+
+        '<form class="form-horizontal">'+
+            '<div class="form-group">'+
+                '<label for="email" class="col-sm-2">Email:</label>'+
+                '<div class="col-sm-10">'+
+                      '<input class="form-control" type="text" name="email"/>'+
+                '</div>'+
+            '</div>'+
+
+            '<div class="form-group">'+
+                '<button class="btn btn-default normal-cta">Subscribe</button>'+
+            '</div>'+
+
+            '<div class="form-group">'+
+                '<button class="btn btn-default alternate-cta">Not Interested?</button>'+
+            '</div>'+
+        '</form>'+
+      '</div>'+
+      '<div class="modal-footer">'+
+        '<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>'+
+      '</div>'+
+    '</div>'+
+  '</div>'+
+'</div>';
+
+
+
+  $('body').append(popup);
+  $('body').append(subscribeModal);
 }
